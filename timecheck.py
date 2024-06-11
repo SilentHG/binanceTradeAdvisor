bca668188008691cbee7ec8174022877cb6048f0
ShakirAli <C9200383@dawlance.com.pk>
2024-06-11 23:25:45 +0500

    first commit

--- timecheck.py
+++ timecheck.py
@@ -12,6 +12,8 @@ TRADE_ACCOUNT_TIME_ZONE = environment_data['TRADE_ACCOUNT_TIME_ZONE'] or 'Europe
 DATA_ACCOUNT_TIME_DICT = environment_data['DATA_ACCOUNT_TIME_DICT'] or {'hours': 0, 'minutes': 0}
 TRADE_ACCOUNT_TIME_DICT = environment_data['TRADE_ACCOUNT_TIME_DICT'] or {'hours': 0, 'minutes': 0}
 
+print("CURRENT_TIME", datetime.now())
+
 print("DATA_ACCOUNT_TIME",
       datetime.now(pytz.timezone(DATA_ACCOUNT_TIME_ZONE)) + timedelta(hours=DATA_ACCOUNT_TIME_DICT['hours'],
                                                                       minutes=DATA_ACCOUNT_TIME_DICT['minutes']))
